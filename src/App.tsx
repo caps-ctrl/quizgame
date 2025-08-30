b03ae606551b86235eff8b48b3842259eb3055d2
Marcel Gusiew <gusiew9@gmail.com>
2025-08-30 23:10:59 +0200

    add meta

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useRoutes } from "react-router-dom";
-
+import { Helmet } from "react-helmet";
 import RootLayout from "./layouts/RootLayout";
 import QuizHomePage from "./pages/QuizHomePage";
 import QuizPage from "./pages/QuizPage";
@@ -24,6 +24,31 @@ function AppRouter() {
 export default function App() {
   return (
     <BrowserRouter>
+      <Helmet>
+        {/* Podstawowe meta */}
+        <title>Quiz Wygral – Sprawdź swoją wiedzę!</title>
+        <meta
+          name="description"
+          content="Rozwiąż quiz, zdobywaj punkty i rywalizuj na tablicy wyników. Różne kategorie i poziomy trudności!"
+        />
+        <meta
+          name="keywords"
+          content="quiz, gra, wiedza, punkty, ranking, edukacja"
+        />
+        <meta name="author" content="Quiz Wygral" />
+
+        {/* Kolor paska adresu / motyw przeglądarki */}
+        <meta
+          name="theme-color"
+          content="#ffffff"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#1e293b"
+          media="(prefers-color-scheme: dark)"
+        />
+      </Helmet>
       <AppRouter />
     </BrowserRouter>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import LoadingPage from "./pages/LoadingPage";
-import { ThemeProvider } from "./components/ThemeProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
